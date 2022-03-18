0a74bf4d3731f672c16693b33ab327af38317fe5
Timur <100406268+Timik232@users.noreply.github.com>
2022-03-18 11:41:33 +0300

    Add files via upload

--- siaode3/siaode3.cpp
+++ siaode3/siaode3.cpp
@@ -1,4 +1,4 @@
-﻿//Дано предложение, состоящее из слов, разделенных запятой или пробелами.Найти самое длинное слово палиндром.
+//Дано предложение, состоящее из слов, разделенных запятой или пробелами.Найти самое длинное слово палиндром.
 //Заменить слово в предложении числом, определяющим его номер среди палиндромов предложения.
 #include <string>
 #include <vector>
@@ -6,17 +6,16 @@
 using namespace std;
 string Input() //ввод строки
 {
-    cout << "Enter sentence, in the end write point\n";
+    cout << "Enter sentence\n";
     string sent;
     getline(cin, sent);
-    cout << sent;
+    //cout << sent; //проверка что вводится
     return sent;
 }
 vector <string> Words(string sent) //разбить на слова
 {
     vector <string> words;
     string buf = "";
-    int count = 0;
     for (int i = 0; i < sent.size(); i++)
     {
         if (sent[i] != ',' && sent[i] != ' ' && sent[i]!='.')
@@ -29,6 +28,8 @@ vector <string> Words(string sent) //разбить на слова
             buf = "";
         }
     }
+    if (buf!="")
+        words.push_back(buf);
     return words;
 }
 void Output(vector <string> word) // вывести слова
@@ -38,50 +39,51 @@ void Output(vector <string> word) // вывести слова
         cout << word[i] << ' ';
     }
 }
-string max_Palindrom(vector <string> word)
+string max_Palindrom(vector <string> word, int nomer[100], int &count) // нахождение и вывод самого длинного палиндрома
 {
     int maxpal = 0;
-    int nomer_max = 0;
+    int nomer_max = -1;
     for (int i = 0; i < word.size(); i++)
     {
         string obr = "";
-        for (int j = word[i].size(); j > 0; j--)
+        for (int j = word[i].size()-1; j >= 0; j--)
         {
             obr.push_back(word[i][j]);
         }
-        if (obr == word[i]) 
-            if (word[i].size() < maxpal)
+        //cout << obr << " " << word[i] << endl; //вывод обратных слов для проверки
+        if (obr == word[i])
+        {
+            nomer[count] = i;
+            count += 1;
+            if (word[i].size() > maxpal)
             {
                 maxpal = word[i].size();
                 nomer_max = i;
             }
-    }
-    return word[nomer_max];
-}
-void Palindrome(vector <string> word)
-{
-    int nomer[100];
-    int count = 0;
-    for (int i = 0; i < word.size(); i++)
-    {
-        string obr = "";
-        for (int j = word[i].size(); j >= 0; j--)
-            obr += word[i][j];
-        if (obr == word[i])
-        {
-            word.at(i) = i;
-            cout << word[i] << ' ';
         }
-
+    }
+    if (nomer_max!=-1)
+        return word[nomer_max];
+    else return "[none]";
+}
+void Palindrome(vector <string> &word, int nomer[100], int count) //замена палиндромов на их номер
+{
+    for (int i = 0; i < count; i++)
+    {
+        word.erase(word.begin() + nomer[i]);
+        word.insert(word.begin()+nomer[i], to_string(i));
+        //cout << word[i] << ' ';
     }
 }
 int main()
 {
+    int nomer[100];
+    int count = 0;
     vector <string> words;
     string sentence = Input();
     words = Words(sentence);
-    //Output(words);
-    cout << "The longest palindrome " << max_Palindrom(words) << endl;
-    //Palindrome(words);
+    //Output(words); //вывод разделённых слов (для проверки)
+    cout << "The longest palindrome " << max_Palindrom(words, nomer, count) << endl;
+    Palindrome(words, nomer, count);
+    Output(words);
 }
-
